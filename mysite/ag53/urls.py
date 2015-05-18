384614f56dbab5b4c776cc34e19ce8b8cb65da6f
kb0304 <karan@Karans-MacBook-Air.local>
2015-05-18 08:00:55 +0530

    log out view added

--- mysite/ag53/urls.py
+++ mysite/ag53/urls.py
@@ -3,4 +3,5 @@ from ag53 import views
 
 urlpatterns = patterns('', url(r'^$', 'django.contrib.auth.views.login', {'template_name': 'ag53/login.html'}),
     url(r'^profile/$', views.profile_view, name='profile'),
+    url(r'^profile/logout$', views.logout_view, name='logout_url')
 )
--- mysite/ag53/views.py
+++ mysite/ag53/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.models import User
+from django.contrib.auth.views import login
 import os
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 @login_required
+
 def profile_view(request):
     u = request.user
     context = {'user':u, 'base_path': os.path.join(BASE_DIR,''),'pp':u.profile.profile_pic,'cp':u.profile.cover_pic}
     return render(request, 'ag53/profile.html', context)
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect('/ag53/')
