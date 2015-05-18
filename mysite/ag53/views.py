167485b1fc0ac77df50373d0639d893d039fdd08
kb0304 <karan@Karans-MacBook-Air.local>
2015-05-18 07:36:30 +0530

    Little bit change

--- mysite/ag53/views.py
+++ mysite/ag53/views.py
@@ -3,9 +3,10 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.contrib.auth.models import User
-
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 @login_required
 def profile_view(request):
     u = request.user
-    context = {'user':u,}
+    context = {'user':u, 'base_path': os.path.join(BASE_DIR,''),'pp':u.profile.profile_pic,'cp':u.profile.cover_pic}
     return render(request, 'ag53/profile.html', context)
